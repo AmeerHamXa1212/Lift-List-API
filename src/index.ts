84689c062c1e5c228695ba86e3c027491f0a1e93
AmeerHamXa1212 <ameer.hamza@ocloudsolutions.net>
2023-09-07 15:11:07 +0500

    Port Updated - Deployment 3

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 import mongoose from "mongoose";
 
-const Port = process.env.PORT;
+const Port = process.env.PORT || 3000;
 mongoose.set("strictQuery", true);
 const server = app.listen(Port, () => {
   console.log(`Server is running at http://localhost:${Port}`);
