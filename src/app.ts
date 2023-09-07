7c51e11e1be38accb8c1b67cc51cc1031b64840a
AmeerHamXa1212 <ameer.hamza@ocloudsolutions.net>
2023-09-07 11:11:29 +0500

    Project Setup

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import mongoose from "mongoose";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("trust proxy", false);
+
+// Use the routes
+// app.use("/api");
+
+const DBConnectionString = process.env.DB_URL;
+const Port = process.env.PORT;
+
+if (!DBConnectionString) {
+  throw new Error("DB_URL environment variable is not defined.");
+}
+if (!Port) {
+  throw new Error("port environment variable is not defined.");
+}
+mongoose
+  .connect(DBConnectionString)
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.log(`Error in DB connection : ${err}`))
+  .finally(() => console.log("This is finally block of code"));
+
+export default app;
